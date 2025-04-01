1e21a6c4c4429dcefe1460d1364c5f8c85fbdd30
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2025-04-01 13:41:45 +0000

    fix: Logic was broken basically, now it should work

--- number_guess.sh
+++ number_guess.sh
@@ -6,32 +6,33 @@ read USERNAME
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 if [[ -z $USER_ID ]]
 then
-  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 1001)")
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
-  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games GROUP BY user_id HAVING user_id=$USER_ID")
-  BEST_GAME=$($PSQL "SELECT MIN(n_guesses) FROM games GROUP BY user_id HAVING user_id=$USER_ID")
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users where user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT best_game from users where user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 NUM_OF_GUESSES=0
 echo "Guess the secret number between 1 and 1000:"
-GUESS=$SNUMBER-1
-while [[ $GUESS -eq $SNUMBER ]]
+echo $SNUMBER
+GUESS=-1
+while [[ $GUESS -ne $SNUMBER ]]
 do
   (( NUM_OF_GUESSES++ ))
   read GUESS
-  if ![[ $GUESS =~ ^-?[0-9]+$ ]]
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
   elif [[ $GUESS -lt $SNUMBER ]]
   then
-    echo "It's lower than that, guess again:"
-  else 
     echo "It's higher than that, guess again:"
+  else 
+    echo "It's lower than that, guess again:"
   fi
 done
 echo "You guessed it in $NUM_OF_GUESSES tries. The secret number was $SNUMBER. Nice job!"
-USER_ID=$($PSQL "select user_id from users where username="$USERNAME"")
+USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
 UPDATE_GAME_COUNT=$($PSQL "update users set games_played=games_played+1 where user_id=$USER_ID")
 UPDATE_BEST_GAME=$($PSQL "update users set best_game=least(best_game, $NUM_OF_GUESSES) where user_id=$USER_ID")
